04e8dcab29e622659f997ef9732ce15e4dfb7980
Shishir Suman <74050813+shishirsuman092@users.noreply.github.com>
2025-09-26 14:24:02 +0530

    adding logs as shared by vikrant

--- data-pipeline-flink/rating/src/main/scala/org/sunbird/dp/rating/function/RatingFunction.scala
+++ data-pipeline-flink/rating/src/main/scala/org/sunbird/dp/rating/function/RatingFunction.scala
@@ -130,11 +130,10 @@ class RatingFunction(config: RatingConfig, @transient var cassandraUtil: Cassand
       }
     }
     catch {
-      case ex: Exception => {
-        ex.printStackTrace()
+      case ex: Exception => 
         context.output(config.failedEvent, event)
-        logger.info("Event throwing exception: ", ex.getMessage)
-      }
+        // log full message + stack trace through logger
+        logger.error(s"Exception while processing event for activityId=${event.activityId}", ex)
     }
   }
 
